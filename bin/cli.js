0c2e46f5bd06f783338d8a79efe2feb67cb762df
Yaroslav <y.surilov@infomir.com>
2020-03-13 14:42:41 +0200

    refactor

--- bin/cli.js
+++ bin/cli.js
@@ -6,119 +6,32 @@
 
 'use strict';
 
-const fs   = require('fs');
-const path = require('path');
-
 /** @see {@link https://www.npmjs.com/package/commander} */
 const commander = require('commander');
 
-/** @see {@link https://www.npmjs.com/package/windows-1251} */
-const windows1251 = require('windows-1251');
-
-/** @see {@link https://www.npmjs.com/package/got} */
-const got = require('got');
-
-/** @see {@link https://www.npmjs.com/package/cheerio} */
-const cheerio = require('cheerio');
-
-const composer = new (require('../lib/composer'));
-const progress = new (require('../lib/progress'));
-
-const packageConfig = require('../package.json');
-
-const DIRECTORY_NAME     = 'backups';
-const FILE_NAME          = `stihi-ru-backup_${new Date().toISOString().slice(0, 10)}.txt`;
-const BASE_URL           = 'https://stihi.ru';
-const OFFSET             = 50;
-const POEM_URL_SELECTOR  = '.poemlink';
-const POEM_TEXT_SELECTOR = '.text';
+const App = require('../lib/app');
 
 commander
-    .version(packageConfig.version)
+    .version(require('../package.json').version)
     .usage('[options] <account name>')
     .option('-r, --reverse-order', 'sort the poems in date descending order')
     .parse(process.argv);
 
-const ACCOUNT = commander.args.pop() || (() => {
-    console.log('You must to pass account name as an argument!');
+const app = new App({
+    accountName:  commander.args.pop(),
+    reverseOrder: commander.reverseOrder
+});
+
+app.isAccountPresent()
+    .then(async () => await app.startScan())
+    .catch(console.error);
+
+//process.on('exit', wamp.call.bind(wamp, 'controlPointDisconnect'));
+
+//['SIGINT', 'SIGTERM'].forEach(signal => process.on(signal, process.exit));
+
+process.on('uncaughtException', error => {
     process.exit(1);
-})();
-
-const isReverseOrderEnabled = commander.reverseOrder;
-
-const pageUrl     = `${BASE_URL}/avtor/${ACCOUNT}`;
-const backupsPath = path.join(__dirname, DIRECTORY_NAME, FILE_NAME);
-
-const decode = buffer => windows1251.decode(buffer.toString('binary'));
-
-let poemLinks = [];
-
-let $;
+});
 
 console.log('Please, wait...');
-progress.startLoop();
-
-(async () => {
-    try {
-        const response = decode(await load(pageUrl));
-
-        let amount = response.replace(/^[\S\s]*Произведений: <b>(\d+)[\S\s]*$/, '$1');
-
-        parse(response);
-        composer.addTitle($('h1').text(), ACCOUNT, pageUrl, amount);
-
-        amount = ~~(amount / OFFSET);
-
-        for ( let index = 1; index <= amount; index += 1 ) {
-            parse(await load(`${pageUrl}&s=${index * OFFSET}`));
-        }
-
-        amount = poemLinks.length;
-
-        for ( let index = 0; index < amount; index += 1 ) {
-            const response = decode(await load(poemLinks[index]));
-
-            composer.addPoem(...parse(response, true), poemLinks[index]);
-        }
-
-        writeFile(composer.getResult());
-    } catch ( error ) {
-        console.log(error.response);
-    }
-})();
-
-
-async function load ( url ) {
-    return got(url, {
-        // need only body content
-        resolveBodyOnly: true,
-        responseType:    'buffer'
-    });
-}
-
-
-function parse ( data, isPoem = false ) {
-    $ = cheerio.load(data);
-
-    if ( isPoem ) {
-        return [
-            $('h1').text(),
-            $(POEM_TEXT_SELECTOR).text()
-        ];
-    } else {
-        $(POEM_URL_SELECTOR).each((index, element) => {
-            poemLinks.push(BASE_URL + $(element).attr('href'));
-        });
-    }
-}
-
-
-function writeFile ( content ) {
-    fs.writeFile(backupsPath, content, 'utf8', error => {
-        if ( error ) {
-            return console.log(`Something went wrong during created your backup.\n${error.message}`);
-        }
-
-        console.log('Your backup is created successfully!');
-    });
-}
