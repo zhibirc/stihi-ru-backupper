c4cfb63a5fcd4028eb905d60a30c9d5b8ff2348f
Yaroslav <y.surilov@infomir.com>
2020-04-10 20:08:25 +0300

    add choosing target folder on PC to save

--- desktop/pages/main/main.js
+++ desktop/pages/main/main.js
@@ -2,12 +2,31 @@
 
 const {dialog} = require('electron').remote;
 
-dialog.showOpenDialog({
-    title: 'Where should I store your backup? Please, specify the directory.',
-    properties: ['openDirectory']
-}).then(result => {
-    console.log(result.canceled)
-    console.log(result.filePaths)
-}).catch(err => {
-    console.log(err)
+let dialogOpened = false;
+
+const $directoryPathToSave   = document.getElementById('directoryPathToSave');
+const $buttonChooseDirectory = document.getElementById('buttonChooseDirectory');
+
+$buttonChooseDirectory.addEventListener('click', () => {
+    if ( dialogOpened ) return;
+
+    dialog.showOpenDialog({
+        title: 'Where should I store your backup? Please, specify the directory.',
+        properties: ['openDirectory']
+    }).then(result => {
+        dialogOpened = false;
+
+        console.log(result.canceled);
+        console.log(result.filePaths);
+
+        if ( !result.canceled && Array.isArray(result.filePaths) ) {
+            $directoryPathToSave.value = result.filePaths[0];
+        }
+    }).catch(error => {
+        dialogOpened = false;
+
+        console.log(error);
+    });
+
+    dialogOpened = true;
 });
